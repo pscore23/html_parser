76962c4904b2bab2f985076d9c214c5a814e9e0c
pscore23 <rubik2357x@gmail.com>
2022-09-29 18:22:48 +0900

    変数の追加

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HTMLParser {
+﻿using System.Collections.Generic;
+
+namespace HTMLParser {
     public class Parser {
         private int dataIndex = 0;
         private int reverseIndex = 0;
@@ -9,5 +11,6 @@
         private string tagAttr;
         private string mainData;
         private string[] emptyTagList = new[] { "area", "base", "basefont", "br", "col", "frame", "hr", "img", "input", "isindex", "link", "meta", "param" };
+        private Dictionary<string, string> attrDict = new Dictionary<string, string>();
     }
 }
